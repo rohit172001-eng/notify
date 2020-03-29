b1dcb02d9e5ff56086641708059d736a1b2d4904
Rohit Lingala <56271857+rohit172001-eng@users.noreply.github.com>
2020-03-29 21:21:33 +0530

    retaining input values after submission
    if password is incorrect values are retained

--- action.php
+++ action.php
@@ -6,13 +6,51 @@ if(isset($_POST['submit']))
 {
   $pass=$_POST["password"];
   $cpass=$_POST["confirmpassword"];
+  $fname=$_POST["fname"];
+  $lname=$_POST["lname"];
+  $email=$_POST["email"];
   if ($pass!=$cpass) {
     alert("PASSWORDS NOT SAME");
     // header("Location:kp.html");
-    header( "Refresh:0; url=index.html", true, 303);
+    // header( "Refresh:0; url=index.html", true, 303);
+    ?>
+    <!DOCUMENT html>
+    <html>
+        <head>
+            <title>Registration form </title>
+        </head>
+        <body>
+            <form style="font:georgia" action="#" method="post">
+                <fieldset>
+
+                    <legend align ="center">Register</legend><br>
+                    <pre>
+                     <label for="fname" >First name:</label>
+                     <input type="text"  name="fname" placeholder="First name" value="<?php print $fname;?>" required><br>
+                     <label for="fname">Last name:</label>
+                     <input type="text" id="lname" name="lname" placeholder="Lastname" value="<?php print $lname;?>" required><br>
+                     <label for="email" >email:</label>a
+                     <input type="text" id="email" name="email" placeholder="mymail@gmail.com" value="<?php print $email;?>" required><br>
+                     <label for="password">Password:</label>
+                     <input type ="password" id="password" name="password" placeholder="Password" pattern=".{8,}"
+                                                                  title="minimum 8 characters"required><br>
+                     <label for="confirmpassword">Confirm Password:</label>
+                     <input type ="password"  name="confirmpassword" placeholder="Confirm Password" required><br>
+                     <input type="submit" name="submit" value="Register">
+                    </pre>
+                </fieldset>
+            </form>
+
+        </body>
+    </html>
+
+    <?php
+    // code...
+  }
+  else {
+    alert("REGISTRATION SUCCESSFULLL");
     // code...
   }
 
-  echo $pass;
 }
  ?>
