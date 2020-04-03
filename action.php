123c35d132c1781748533bf47091da5e0bf5a645
kp-debug <keerthipriya1438@gmail.com>
2020-04-03 13:46:39 +0530

    Update action.php

--- action.php
+++ action.php
@@ -14,35 +14,37 @@ if(isset($_POST['submit']))
     // header("Location:kp.html");
     // header( "Refresh:0; url=index.html", true, 303);
     ?>
-    <!DOCUMENT html>
-    <html>
-        <head>
-            <title>Registration form </title>
-        </head>
-        <body>
-            <form style="font:georgia" action="#" method="post">
-                <fieldset>
+<!DOCUMENT html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Registration form </title>
+        <link rel="stylesheet" href="style.css">
+    </head>
 
-                    <legend align ="center">Register</legend><br>
-                    <pre>
-                     <label for="fname" >First name:</label>
-                     <input type="text"  name="fname" placeholder="First name" value="<?php print $fname;?>" required><br>
-                     <label for="fname">Last name:</label>
-                     <input type="text" id="lname" name="lname" placeholder="Lastname" value="<?php print $lname;?>" required><br>
-                     <label for="email" >email:</label>a
-                     <input type="text" id="email" name="email" placeholder="mymail@gmail.com" value="<?php print $email;?>" required><br>
-                     <label for="password">Password:</label>
-                     <input type ="password" id="password" name="password" placeholder="Password" pattern=".{8,}"
-                                                                  title="minimum 8 characters"required><br>
-                     <label for="confirmpassword">Confirm Password:</label>
-                     <input type ="password"  name="confirmpassword" placeholder="Confirm Password" required><br>
-                     <input type="submit" name="submit" value="Register">
-                    </pre>
-                </fieldset>
-            </form>
-
-        </body>
-    </html>
+    <body >
+        <img src="https://images.mentalfloss.com/sites/default/files/styles/mf_image_16x9/public/headerbooks.jpg?itok=VXk5ovFG&resize=1100x1100" 
+                                                                                     width="100%" height="1060%" >
+        <form class="box" action="#" method="post">
+            <fieldset padding="20px"> 
+                <h1 >Register</h1>
+                <hr></hr>
+                 <label for="fname" >First name:</label>
+                 <input type="text" name="fname" placeholder="First name" value="<?php print $fname;?>" autofocus required>
+                 <label for="fname">Last name:</label>
+                 <input type="text" name="lname" placeholder="Lastname" value="<?php print $lname;?>" required>
+                 <label for="email" >email:</label>
+                 <input type="text" name="email" placeholder="mymail@gmail.com" value="<?php print $email;?>" required>
+                 <label for="password">Password:</label>
+                 <input type ="password" name="password" placeholder="Password" pattern=".{8,}"
+                                                                         title="minimum 8 characters" required>
+                 <label for="confirmpassword">Confirm Password:</label>
+                 <input type ="password" name="confirmpassword" placeholder="Confirm Password" required><br>
+                 <input type="submit" name="submit" value="Register" >
+            </fieldset>
+        </form>
+    </body>
+</html>
 
     <?php
     // code...
